ce7f7cd465a2b007941548a848e061685c7cbbf6
Raymond Starkey <starkeyrb@outlook.com>
2024-12-25 16:14:44 +0000

    Test 1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+//https://learn.microsoft.com/en-us/azure/static-web-apps/application-settings
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
